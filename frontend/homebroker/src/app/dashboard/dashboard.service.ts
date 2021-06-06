6bbecc9a536785c064573d0ddc2c2743c0f7f23e
Ludmylla Arielly <ludmay4@gmail.com>
2021-06-06 14:42:12 -0300

    Create stock service

--- frontend/homebroker/src/app/dashboard/dashboard.service.ts
+++ frontend/homebroker/src/app/dashboard/dashboard.service.ts
@@ -7,11 +7,12 @@ import Stock from '../shared/models/stock-model';
 })
 export class DashboardService {
 
+  readonly baseUrl = "http://localhost:8080/bootcamp";
+
   constructor(private http: HttpClient) { }
 
   async getStock(): Promise<Stock[]>{
-    const url = " http://localhost:8080/bootcamp/stock";
-    return this.http.get<Stock[]>(url).toPromise();
+    return this.http.get<Stock[]>(`${this.baseUrl}/stock`).toPromise();
    
   }
     
