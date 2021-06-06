56d0b066591fda57431e3ecb19bb088bb66ed44f
Ludmylla Arielly <ludmay4@gmail.com>
2021-06-06 13:53:48 -0300

    create service stock

--- frontend/homebroker/src/app/dashboard/dashboard.service.ts
+++ frontend/homebroker/src/app/dashboard/dashboard.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import Stock from '../shared/models/stock-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  async getStock(): Promise<Stock[]>{
+    const url = " http://localhost:8080/bootcamp/stock";
+    return this.http.get<Stock[]>(url).toPromise();
+   
+  }
+    
 }
